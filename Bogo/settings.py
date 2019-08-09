72fb1836ffc4c994a69d057f95fc51d638708d33
QuatreMains <plutokor@gmail.com>
2019-08-10 03:12:30 +0900

    mod set

--- Bogo/settings.py
+++ Bogo/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'parsed_data/static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
